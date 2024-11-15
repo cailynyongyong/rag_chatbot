234542fce873d2163e58bf388fbda47bac5bf7a7
Cailyn Yong <cailynyongyong@gmail.com>
2024-11-15 12:13:05 +0900

    f

--- chatbot.py
+++ chatbot.py
@@ -1,6 +1,6 @@
-__import__('pysqlite3')
-import sys
-sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
+# __import__('pysqlite3')
+# import sys
+# sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
 # 사용할 라이브러리 선언
 import streamlit as st
 import openai
